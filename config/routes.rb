6d638056e49458293291aeceb17eb8628476923a
Jason Potter <jasonpotter92@me.com>
2021-02-10 11:36:39 +0000

    bugs

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :contacts, only: :create
   get 'contact-us', to: 'contacts#new', as: 'new_contact'
-  get 'contacts', to: 'contacts#new', as: 'new_contact'
 end
